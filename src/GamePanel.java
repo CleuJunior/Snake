b0db5af2fc1be5cdd942db2f01eab302a3bf5b10
CleuJunior <cleu.junior@gmail.com>
2021-01-10 12:48:30 -0300

    First Commit

---
+++ src/GamePanel.java
@@ -0,0 +1,40 @@
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.event.ActionListener;
+
+import java.awt.event.ActionEvent;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+        
+    }
+
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+        
+    }
+}
